880ad5fc3ec25a68ef4ed55449fe498b63bf084f
Eli <68206794+tony-rbxl@users.noreply.github.com>
2022-08-03 20:14:44 -0400

    edit

--- LocationTitle.lua
+++ LocationTitle.lua
@@ -35,67 +35,67 @@ local PlayerGui = localPlayer.PlayerGui
 
 
 function module:Play(Text)
-    local function text(num)
-        TweenService:Create(module.TextLabel, TweenInfo.new(module.TweenLengthText, Enum.EasingStyle.Sine,Enum.EasingDirection.Out), {TextTransparency = num}):Play()
-    end
+	local function text(num)
+		TweenService:Create(module.TextLabel, TweenInfo.new(module.TweenLengthText, Enum.EasingStyle.Sine,Enum.EasingDirection.Out), {TextTransparency = num}):Play()
+	end
 
-    local function other(time, num)
-        for _, guiObject in pairs(module.MainFrame:GetDescendants()) do
-            if guiObject:GetAttribute("enabled") == true then
-                if time == "opening" then
-                    if guiObject:IsA("Frame") then
-                        TweenService:Create(guiObject,TweenInfo.new(module.TweenLengthOther, Enum.EasingStyle.Sine,Enum.EasingDirection.Out), {Size = guiObject:GetAttribute("sizeStart"), BackgroundTransparency = num}):Play()
-                    end
-                    if guiObject:IsA("ImageLabel") then
-                        TweenService:Create(guiObject,TweenInfo.new(module.TweenLengthOther, Enum.EasingStyle.Sine,Enum.EasingDirection.Out), {Size = guiObject:GetAttribute("sizeStart"), ImageTransparency = num}):Play()
-                    end
-                end
-        
-                if time == "closing" then
-                    if guiObject:IsA("Frame") then
-                        TweenService:Create(guiObject,TweenInfo.new(module.TweenLengthText, Enum.EasingStyle.Sine,Enum.EasingDirection.Out), {Size = guiObject:GetAttribute("sizeClose"), BackgroundTransparency = num}):Play()
-                    end
-                    if guiObject:IsA("ImageLabel") then
-                        TweenService:Create(guiObject,TweenInfo.new(module.TweenLengthText, Enum.EasingStyle.Sine,Enum.EasingDirection.Out), {Size = guiObject:GetAttribute("sizeClose"), ImageTransparency = num}):Play()
-                    end
-                end
-            end
-        end
-    end
+	local function other(time, num)
+		for _, guiObject in pairs(module.MainFrame:GetDescendants()) do
+			if guiObject:GetAttribute("enabled") == true then
+				if time == "opening" then
+					if guiObject:IsA("Frame") then
+						TweenService:Create(guiObject,TweenInfo.new(module.TweenLengthOther, Enum.EasingStyle.Sine,Enum.EasingDirection.Out), {Size = guiObject:GetAttribute("sizeOpen"), BackgroundTransparency = num}):Play()
+					end
+					if guiObject:IsA("ImageLabel") then
+						TweenService:Create(guiObject,TweenInfo.new(module.TweenLengthOther, Enum.EasingStyle.Sine,Enum.EasingDirection.Out), {Size = guiObject:GetAttribute("sizeOpen"), ImageTransparency = num}):Play()
+					end
+				end
 
-    module.TextLabel.Text = Text
-    module.MainSound:Play()
-    other("opening", 0)
+				if time == "closing" then
+					if guiObject:IsA("Frame") then
+						TweenService:Create(guiObject,TweenInfo.new(module.TweenLengthText, Enum.EasingStyle.Sine,Enum.EasingDirection.Out), {Size = guiObject:GetAttribute("sizeClose"), BackgroundTransparency = num}):Play()
+					end
+					if guiObject:IsA("ImageLabel") then
+						TweenService:Create(guiObject,TweenInfo.new(module.TweenLengthText, Enum.EasingStyle.Sine,Enum.EasingDirection.Out), {Size = guiObject:GetAttribute("sizeClose"), ImageTransparency = num}):Play()
+					end
+				end
+			end
+		end
+	end
 
-    task.wait(module.TweenLengthOther)
-    text(0)
-    task.wait(module.TweenLengthText + 1)
-    text(1)
+	module.TextLabel.Text = Text
+	module.MainSound:Play()
+	other("opening", 0)
 
-    other("closing", 1)
+	task.wait(module.TweenLengthOther)
+	text(0)
+	task.wait(module.TweenLengthText + 1)
+	text(1)
+
+	other("closing", 1)
 
 end
 
 
 function module:Init(Settings)
-    module.TweenLengthOther = Settings.TweenLengthOther or 10
-    module.TweenLengthText = Settings.TweenLengthText or 4
-    module.MainGui = Settings.GUI or script:WaitForChild("Template")
-    module.TextFont = Settings.TextFont or Enum.Font.Bodoni
+	module.TweenLengthOther = Settings.TweenLengthOther or 10
+	module.TweenLengthText = Settings.TweenLengthText or 4
+	module.MainGui = Settings.GUI or script:WaitForChild("Template")
+	module.TextFont = Settings.TextFont or Enum.Font.Bodoni
 
-    if not module.MainGui:IsDescendantOf(PlayerGui) then
-        module.MainGui = module.MainGui:Clone()
-        module.MainGui.Parent = PlayerGui
-        module.MainGui.Name = "LocationTitle"
-    end
+	if not module.MainGui:IsDescendantOf(PlayerGui) then
+		module.MainGui = module.MainGui:Clone()
+		module.MainGui.Parent = PlayerGui
+		module.MainGui.Name = "LocationTitle"
+	end
 
-    module.MainFrame = module.MainGui:WaitForChild("MainFrame")
-    module.TextLabel = module.MainFrame:WaitForChild("LocationLabel")
+	module.MainFrame = module.MainGui:WaitForChild("MainFrame")
+	module.TextLabel = module.MainFrame:WaitForChild("LocationLabel")
 
-    module.MainSound = Instance.new("Sound")
-    module.MainSound.Parent = module.MainGui
-    module.MainSound.SoundId = Settings.SongId or "rbxassetid://1846898632"
-    module.MainSound.Volume = Settings.Volume or 0.5
+	module.MainSound = Instance.new("Sound")
+	module.MainSound.Parent = module.MainGui
+	module.MainSound.SoundId = Settings.SongId or "rbxassetid://1846898632"
+	module.MainSound.Volume = Settings.Volume or 0.5
 end
 
 return module
